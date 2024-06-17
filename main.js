3220dd741d6825185a458489490ef3c90495daa9
ignacio lopez rodriguez <igju06@hotmail.com>
2024-06-17 20:24:24 -0300

    agregue imagenes default y limite el array a 100

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ fetch("https://hp-api.onrender.com/api/characters")
 .then((respuesta)=> {return respuesta.json()})
 .then((data)=>{
     console.log(data);
-    const personajes = data
+    const personajes = data.slice(0,100)
     console.log(personajes);
 
     personajes.forEach((personaje) => {
-        $divPersonajes.innerHTML += `
+        $divPersonajes.innerHTML += `<div>
         <h4>${personaje.name}</h4>
-        <img src="${personaje.image}" alt="imagen de: ${personaje.name}">
-        <p>${personaje.actor}</p>
-        <p>${personaje.gender}</p>
-        <p>${personaje.house}</p>
-        <p>${personaje.dateOfBirth}</p>
+        
+        <img src="${personaje.image}" onerror="this.src='../imagenError.png.png'" alt="imagen de: ${personaje.name}">
+        
+        <p>Actor: ${personaje.actor}</p>
+        <p>Genero: ${personaje.gender}</p>
+        <p>Casa: ${personaje.house}</p>
+        <p>Fecha de nacimiento: ${personaje.dateOfBirth}</p>
+        </div>
         `
     });
 })
